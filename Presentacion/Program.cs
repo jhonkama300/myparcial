e5e925a832677f0c818d25163a0c7625f53f0d50
jhonkama300 <jhonkama300@gmail.com>
2020-10-08 14:52:14 -0500

    se confirma el punto 1

---
+++ Presentacion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Policy;
+using System.Runtime.CompilerServices;
+
+namespace Presentacion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
